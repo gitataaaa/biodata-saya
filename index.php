848f9cf3496ae9d86cdc4f12fb4638ce13ef1fdd
Nama Lengkapmu <email-githubmu@example.com>
2026-05-21 08:26:43 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website Gita</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <p>Website pertama saya</p>
+</body>
+</html>
